2f333169892a10232cb03190ca0f373a30acc4cb
Ali Haydar <ali.haydar85@gmail.com>
2019-03-02 10:37:31 +1300

    Add the db folder

---
+++ db/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const NeDB = require('nedb');
+
+const filename = path.resolve('db', 'db.json');
+
+const db = new NeDB({
+    filename: filename,
+    autoload: true,
+});
+
+const data = {
+    find: (query) => new Promise((resolve, reject) =>
+        db.find(query)
+        .exec((err, docs) => {
+            if (err) reject(err);
+            return resolve(docs);
+        })
+    )
+}
+
+module.exports = data;
