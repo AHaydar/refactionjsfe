4797f7bfa0d812479740de6335b00987cafa5de8
Ali Haydar <ali.haydar85@gmail.com>
2019-03-05 22:18:01 +1300

    Add age filter type in graphql and handle its transformation to nedb json format in the resolver

--- src/server.js
+++ src/server.js
@@ -6,7 +6,39 @@ const data = require('./db/index');
 const resolvers = {
   Query: {
     info: () => `This is the API of Persons`,
-    people: (root, args, context) => data.find(args)
+    people: (root, args, context) => {
+      const argsJSON = JSON.parse(JSON.stringify(args));
+
+      //transform the passed graphql age input to a json object that is understandable by nedb to filter by age
+      if (argsJSON.age) {
+        switch(argsJSON.age.operator) {
+          case "LT":
+            argsJSON.age = {
+              $lt: args.age.value
+            }
+            break;
+          case "LTE":
+            argsJSON.age = {
+              $lte: args.age.value
+            }
+            break;
+          case "GT":
+            argsJSON.age = {
+              $gt: args.age.value
+            }
+            break;
+          case "GTE":
+            argsJSON.age = {
+              $gte: args.age.value
+            }
+            break;
+          case "EQ":
+            argsJSON.age = args.age.value
+            break;
+        }
+      }
+      return data.find(argsJSON);
+    }
   },
   Person: {
     id: (parent) => parent._id,
@@ -19,3 +51,5 @@ const server = new GraphQLServer({
 })
 
 server.start(() => console.log(`GraphQL Server is running on http://localhost:4000`));
+
+// { gender: 'female', age: { value: 30, operator: 'LT' } }
