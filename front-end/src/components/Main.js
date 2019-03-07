b00997778bc5d9878205d9a367c716c36f198113
Ali Haydar <ali.haydar85@gmail.com>
2019-03-08 06:36:14 +1300

    Fix hash added in url because of HashRouter

--- front-end/src/components/Main.js
+++ front-end/src/components/Main.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, NavLink, HashRouter } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
 import gql from 'graphql-tag';
 import People from './People';
 import queries from '../graphql/queries';
 
 const Main = () => (
-  <HashRouter>
+  <BrowserRouter>
     <div>
       <div>
         <NavLink exact to="/">Everyone</NavLink>
@@ -22,7 +22,7 @@ const Main = () => (
         <Route path="/under30" render={(props) => <People {...props} gql={gql`${queries.under30}`} />}/>
       </div>
     </div>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default Main;
