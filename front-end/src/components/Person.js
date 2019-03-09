b9fd5c111515d2840415a7975ae0f7dec664343f
Ali Haydar <ali.haydar85@gmail.com>
2019-03-09 18:40:56 +1300

    Run lint on files. Set up husky to run lint on pre-commit

--- front-end/src/components/Person.js
+++ front-end/src/components/Person.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Person = ({ person }) => (
-    <li>
-        {person.name} {person.age}
-    </li>
+  <li>
+    {person.name}
+    {' '}
+    {person.age}
+  </li>
 );
+Person.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default Person;
